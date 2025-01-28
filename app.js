067913e56871a81f420f7fc8b4c67a73bb88a681
Sardorbox <akmaljonqodirov1223@gmail.com>
2025-01-28 14:26:32 +0500

    JavaScript code

---
+++ app.js
@@ -0,0 +1,115 @@
+// let ism = "Akmaljon";
+// console.log("Mening ismim" + ism);
+// // first task completed
+
+// let year = 17;
+// console.log(`men ${year} "yoshdaman!`);
+// //second task  completed
+
+// let favColor = "Violet";
+// console.log("Mening yoqtirgan rangim:" +  favColor);
+// //third task  completed
+
+// let num1 = 120;
+// let num2 = 122;
+
+// let plus = num1 + num2;
+// console.log("Yig'indi: " +  plus);
+// //fourth task completed
+
+// let text1 = "JavaScript";
+// let text2 = "Java";
+
+// let text = text1 + " vs " + text2;
+// console.log(text)
+// //fiveth task completed
+
+// let userName = prompt("Ismingizni kiriting:");
+// console.log(`Salom, ${userName}!`);
+// //sixth task completed
+
+// let userAge = prompt("yoshingizni kiriting:");
+// let presentYear = 2025;
+
+// let userAgeValue = presentYear - Number (userAge);
+// console.log(userAgeValue);
+// //seventh task completed
+
+// let userNum1 = prompt("Birinchi raqamni kiriting:");
+// let userNum2 = prompt("ikkinchi raqamni kiriting:");
+
+// let value = Number(userNum1) + Number(userNum2);
+// console.log(value);
+// //eightth task  completed
+
+// let favFruit = prompt("Yoqtirgan mevangizni  kiriting:");
+
+// console.log("Sizning yoqtirgan  mevangiz:" + " " + favFruit);
+// //nineth task  completed
+
+// let radius = prompt("Doiraning radiusini kiriting:");
+// let p = 3.14;
+
+// let yuzasi = p * (Number(radius) ** 2);
+// console.log("Doiraning yuzasi: " + yuzasi);
+// //10th task completed
+
+// let request = confirm("Dasturni davom ettirishni hohlaysizmi?");
+// console.log(request ? "Dastur davom ettirilmoqda..." : " Dastur toxtatildi!");
+// //11th task  completed
+
+let age = prompt("Yoshingizini kiriting:");
+console.log(age > 18 ? "Siz kattasiz!" : "Siz hali yoshsiz!");
+//12th task completed
+
+let password = prompt("Parolni kiriting:");
+console.log(password == 12345 ? "Xush kelibsiz!" : "Parol not'g'ri!");
+//13th task completed
+
+let userFirstName = prompt("Ismingizni kirting:");
+userFirstName ?  console.log( "Salom, " + userFirstName) : console.log("Ism kiritilmadi!");
+//14th task completed
+
+let son1 = prompt("Birinchi raqamni kiriting:");
+let son2 = prompt("ikkinchi raqamni kiriting:");
+
+let kopaytma = Number(son1) * Number(son2);
+console.log(kopaytma);
+//15th task completed
+
+let raqam1 = prompt("Birinchi raqamni kiriting:");
+let raqam2 = prompt("ikkinchi raqamni kiriting:");
+
+let ayirma = Number(raqam1) - Number(raqam2);
+console.log(ayirma);
+//16th task completed
+
+let asosi = prompt("Uchburchakning asosini kiriting:");
+let balandligi = prompt("Uchburchakning balandligi kiriting:");
+
+let S = asosi * balandligi / 2;
+console.log(S);
+//17th task completed
+
+let aylana = prompt("Aylana radiusini kiriting:");
+
+let L = 2 * 3.14 * aylana;
+console.log(L);
+//18th task completed
+
+let first_name = prompt("Ismingizni kiriting:");
+let last_name = prompt("Familiyangizni kiriting:");
+let years_old = Number (prompt("Yoshingizni kiriting:"));
+
+console.log("Ismingiz:" + first_name);
+console.log("Familiyangiz:" + last_name);
+console.log("Yoshingiz:" + years_old);
+//19th task completed
+
+let favmovie1 = prompt(" 3ta Sevimli kinoingizni kiriting!");
+let favmovie2 = prompt(" Sevimli kinoingizni kiriting!");
+let favmovie3 = prompt(" Sevimli kinoingizni kiriting!");
+
+console.log("-1. Sevimli kinoingiz" + " " + favmovie1);
+console.log("-2. Sevimli kinoingiz" + " " + favmovie2);
+console.log("-3. Sevimli kinoingiz" + " " + favmovie3);
